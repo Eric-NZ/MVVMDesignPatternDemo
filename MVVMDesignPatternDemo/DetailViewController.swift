db511582167b3986614c8dd4a93c347873488279
LomoCode <lomocode@Lomobook.local>
2018-09-29 10:24:02 +1200

    Initial Commit

---
+++ MVVMDesignPatternDemo/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  MVVMDesignPatternDemo
+//
+//  Created by Eric Ho on 29/09/18.
+//  Copyright © 2018 LomoStudio. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
